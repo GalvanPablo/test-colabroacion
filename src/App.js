78ddf95e4e4b5d9e064d1c3ae8179f2599be953a
Pablo Galvan <pablogalvan.015@gmail.com>
2023-05-23 13:19:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Header } from './components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header title="Hola Cañom"/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam blanditiis a quos! Incidunt vitae illo accusantium suscipit possimus numquam cupiditate est quas. Explicabo maxime aperiam quia repellat recusandae exercitationem?</p>
+        </div>
+    );
 }
 
 export default App;
