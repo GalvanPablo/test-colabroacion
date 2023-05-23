d98f72d52f98b5e6b400050dcdf90ba5f9ef2eb6
Lautaro1901 <111198082+Lautaro1901@users.noreply.github.com>
2023-05-23 13:46:17 -0300

    Revert "feat - Imagen añadida"

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
         <div className="App">
             <Header title="Hola Cañom"/>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem nam blanditiis a quos! Incidunt vitae illo accusantium suscipit possimus numquam cupiditate est quas. Explicabo maxime aperiam quia repellat recusandae exercitationem?</p>
-            <img src="https://media.discordapp.net/attachments/906996552629891112/1102024557302403102/image.png?width=1320&height=701" alt="imagen" />
         </div>
     );
 }
